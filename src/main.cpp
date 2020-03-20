9b0990e8c83e9917996552700bb171d6c922baf9
Tamas Boldizsar <boldizsartomi96@gmail.com>
2020-03-20 18:57:27 +0100

    Extend main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+
 #include <QDebug>
+
+#include "example.h"
 
 int main()
 {
 	qDebug() << "Travis app started...";
+
+    std::cout << example::sum(2, 5) << std::endl;
+    std::cout << example::multiply(2, 5) << std::endl;
 
 	return 0;
 }

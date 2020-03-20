88742c74b8573795ff594775e0b232b754a17637
Tamas Boldizsar <boldizsartomi96@gmail.com>
2020-03-20 19:20:07 +0100

    Convert indentation to tabs

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	qDebug() << "Travis app started...";
 
-    std::cout << example::sum(2, 5) << std::endl;
-    std::cout << example::multiply(2, 5) << std::endl;
+	std::cout << example::sum(2, 5) << std::endl;
+	std::cout << example::multiply(2, 5) << std::endl;
 
 	return 0;
 }
